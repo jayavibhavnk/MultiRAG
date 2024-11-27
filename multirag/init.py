3fc58f529af9dd43f17f89847e41e703a76b4077
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-11-27 00:37:34 -0800

    Create init.py

---
+++ multirag/init.py
@@ -0,0 +1,7 @@
+from multirag.core.encoders import MultiModalEncoder
+from multirag.core.indexing import MultiModalIndex, Document
+from multirag.models.attention import CrossModalAttention
+from multirag.models.memory import ContextualMemory
+from multirag.pipeline.rag_pipeline import MultiModalRAGPipeline
+
+__version__ = "0.1.0"
